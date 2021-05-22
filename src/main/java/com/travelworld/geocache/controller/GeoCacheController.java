e3d6f16bc783e1a4ed095de56a6bbad7a416c9c7
Mejari Chellapandian <geocache.tw@gmail.com>
2021-05-22 19:27:07 +0530

    Gradle config changes

--- src/main/java/com/travelworld/geocache/controller/GeoCacheController.java
+++ src/main/java/com/travelworld/geocache/controller/GeoCacheController.java
@@ -2,14 +2,17 @@ package com.travelworld.geocache.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Controller	
 public class GeoCacheController {
 	
 	
 	@GetMapping("/greeting")
-	public String getData() {
-		return "Welcome To geoCache";
+	public ResponseEntity<String> getData() {
+		System.out.println("Welcome to application");
+		return new ResponseEntity<>("Application Up and running!",HttpStatus.OK);
 		
 	}
 
